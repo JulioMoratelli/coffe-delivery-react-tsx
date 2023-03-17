e12dc88b6c631f2005daf8d08de550a28a5e937e
Julio <l3xpooh@gmail.com>
2023-03-16 23:43:17 -0300

    estrutura de pastas quase finalizada

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './global/default/indexDefault'
+import { Checkout } from './screens/checkout/indexCheckout'
+import { Home } from './screens/home/indexHome'
+import { Success } from './screens/success/indexSuccess'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
+    </Routes>
+  )
+}
