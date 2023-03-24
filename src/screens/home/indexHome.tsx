91b3345a72d43f0ef106dc4d38a9d648828a0b39
Julio Moratelli <l3xpooh@gmail.com>
2023-03-24 16:38:02 -0300

    finalizando estilização home

--- src/screens/home/indexHome.tsx
+++ src/screens/home/indexHome.tsx
@@ -1,6 +1,6 @@
-import { Package } from '@phosphor-icons/react'
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
 import BannerCoffe from '../../../imgs/banner-coffe.svg'
-import { StyleBanner, StyledSubText, StyleTitle, } from './styledHome'
+import { GlobalIcons, StyleBanner, StyledSubText, StyleTitle, } from './styledHome'
 
 
 export function Home() {
@@ -10,22 +10,24 @@ export function Home() {
                 <StyleTitle>Encontre o café perfeito para qualquer hora do dia</StyleTitle>
                 <StyledSubText>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</StyledSubText>
 
-                <div>
-                    <Package size={24}/>
-                    Embalagem mantém o café intacto
-                </div>
-                <div>
-                    <Package size={24}/>
-                    Embalagem mantém o café intacto
-                </div>
-                <div>
-                    <Package size={24}/>
-                    Embalagem mantém o café intacto
-                </div>
-                <div>
-                    <Package size={24}/>
-                    Embalagem mantém o café intacto
-                </div>
+                <GlobalIcons>
+                    <div>
+                        <ShoppingCart size={24}/>
+                        <p>Embalagem mantém o café intacto</p>
+                    </div>
+                    <div>
+                        <Package size={24}/>
+                        <p>Embalagem mantém o café intacto</p>
+                    </div>
+                    <div>
+                        <Timer size={24}/>
+                        <p>Embalagem mantém o café intacto</p>
+                    </div>
+                    <div>
+                        <Coffee size={24}/>
+                        <p>Embalagem mantém o café intacto</p>
+                    </div>
+                </GlobalIcons>
             </div>
 
             <img src={BannerCoffe} alt="" />
--- src/screens/home/styledHome.ts
+++ src/screens/home/styledHome.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 
 export const StyleBanner = styled.article`
     display: flex;
-    align-items: center;
     justify-content: space-around;
-    margin-top: 4rem;
+    margin-top: 3rem;
+    color: ${(props) => props.theme['gray-900']};
 `
 
 export const StyleTitle = styled.p`
@@ -23,4 +23,38 @@ export const StyledSubText = styled.p`
     font-size: 20px;
     padding: 0 0 2rem 0;
     width: 600px;
+    color: ${(props) => props.theme['gray-800']};
+`
+
+//--------------------------------------------------------------------------------------------------
+
+
+export const GlobalIcons = styled.footer`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 600px;
+    gap: 10px;
+    flex-wrap: wrap;
+
+    div{
+        display: flex;
+        padding: 5px;
+    }
+    
+    svg{
+        border-radius: 50%;
+        height: 35px;
+        width: 35px;
+        padding: 7px;
+        background-color: ${(props) => props.theme['purple-dark']};
+    }
+
+    p {
+        font-family: 'Roboto';
+        font-weight: 400px;
+        font-size: 16px;
+        margin-top: auto;
+        margin-bottom: auto;
+    }
 `
