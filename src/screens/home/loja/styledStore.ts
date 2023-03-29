70224bc7ed28d5a522d482631b2f1a23936f85e3
Julio <l3xpooh@gmail.com>
2023-03-28 23:30:04 -0300

    estilizando coffes

--- src/screens/home/loja/styledStore.ts
+++ src/screens/home/loja/styledStore.ts
@@ -2,9 +2,32 @@ import styled from "styled-components";
 
 
 export const SylesStore = styled.div`
+    display: flex ;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
     width: 256px;
     height: 310px;
-    
+    padding: 1rem;
+    border-radius: 6px 36px;
 
     background-color: ${(props) => props.theme['gray-400']};
+`
+
+export const StyledImgCoffe = styled.div`
+  margin-top: calc(0px - 2rem - 6px);
+  
+  p{
+    margin: 10px 15px;
+    padding: 2px 4px;
+    border-radius: 100px;
+
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 10px;
+
+    background-color: ${(props) => props.theme['yellow-light']};
+    color: ${(props) => props.theme['yellow-dark']} ;
+  }
 `
