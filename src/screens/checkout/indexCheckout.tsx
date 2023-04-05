4d6ede7f7c94d13373a52402140737956892988b
Julio Moratelli <l3xpooh@gmail.com>
2023-04-05 16:56:02 -0300

    checkout semi finalizado

--- src/screens/checkout/indexCheckout.tsx
+++ src/screens/checkout/indexCheckout.tsx
@@ -1,38 +1,38 @@
 import { MapPinLine } from "@phosphor-icons/react";
-import { StyledAllCheckout } from "./styledCheckout";
+import { StyledAllCheckout, DeliveryAddress, DivFormAddress } from "./styledCheckout";
 
 export function Checkout() {
   return (
     <StyledAllCheckout>
-      <div>
-        <p>Complete seu pedido</p>
+          <p>Complete seu pedido</p>
 
-        <div>
-          <MapPinLine size={24} />
-          <h3>Endereço de Entrega</h3>
-          <p>Informe o endereço onde deseja receber seu pedido</p>
-          <form action="">
-            <input type="number" placeholder="CEP" required />
-            <input type="text" placeholder="Nome" required />
-            <input type="number" placeholder="Número" required />
-            <input type="text" placeholder="Complemento" />
-            <input type="text" placeholder="Bairro" required/>
-            <input type="text" placeholder="Cidade" required />
-            <input type="text" placeholder="UF" required />
-          </form>
-        </div>
-
-        <div>
-          <h3>Pagamento</h3>
-          <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+        <DeliveryAddress>
+          <DivFormAddress>
+            <MapPinLine size={24} />
+            <h3>Endereço de Entrega</h3>
+            <p>Informe o endereço onde deseja receber seu pedido</p>
+            <form action="">
+              <input type="number" placeholder="CEP" required />
+              <input type="text" placeholder="Nome" required />
+              <input type="number" placeholder="Número" required />
+              <input type="text" placeholder="Complemento" />
+              <input type="text" placeholder="Bairro" required/>
+              <input type="text" placeholder="Cidade" required />
+              <input type="text" placeholder="UF" required />
+            </form>
+          </DivFormAddress>
 
           <div>
-            <button>cartão de crédito</button>
-            <button>cartãod e débito</button>
-            <button>dinheiro</button>
+            <h3>Pagamento</h3>
+            <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+
+            <div>
+              <button>cartão de crédito</button>
+              <button>cartãod e débito</button>
+              <button>dinheiro</button>
+            </div>
           </div>
-        </div>
-      </div>
-    </StyledAllCheckout>
+      </ DeliveryAddress>
+    </ StyledAllCheckout>
   );
 }
--- src/screens/checkout/styledCheckout.ts
+++ src/screens/checkout/styledCheckout.ts
@@ -3,4 +3,55 @@ import styled from "styled-components";
 
 export const StyledAllCheckout = styled.div`
   padding: 5rem 10rem 0rem 10rem;
+  align-items: center;
+
+  
+  p{
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-size: 18px;
+    color: #403937;
+
+  }
+`
+
+export const DeliveryAddress = styled.div`
+  border-radius: 6px;
+  width: 640px;
+  height: 591px;
+`
+
+export const DivFormAddress = styled.div`
+  display: flex;
+  width: 640px;
+  height: 372px; 
+  padding: 2.5rem;
+  flex-wrap: wrap;
+  background: #F3F2F2;
+
+  svg{
+    margin-right: 10px;
+    color: #C47F17;
+    height: 24;
+  }
+
+  h3{
+    height: 0rem;
+  }
+
+  p{
+    font-family: 'Roboto';
+    font-size: 14px;
+    width: 530px;
+    padding-left: 35px;
+    height: 0rem;
+
+    color: #574F4D;
+  }
+
+  form{
+    width: 560px;
+    height: 216px;
+    margin-top: 3rem;
+  }
 `
